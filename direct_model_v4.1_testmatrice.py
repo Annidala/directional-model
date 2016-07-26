188699b8b42f21712828e49bc4205d8c31092028
CMartelLML <annie.morch@gmail.com>
2016-07-26 14:46:49 +0200

    maj fichier modelisation isotrope

--- direct_model_v4.1_testmatrice.py
+++ direct_model_v4.1_testmatrice.py
@@ -1,4 +1,4 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import numpy as np
 import scipy.ndimage as nd
 import scipy.optimize as opt
@@ -64,6 +64,18 @@ def _computeDeriveDirectionStrain(Ftensor, ui):
 	di=(1/_computeDirectionStrain(Ftensor,ui))*np.dot(Ftensor, np.outer(ui,ui))
 	return di
 
+def _computeModeledDerivedEnergy(strain,theta,u,p):
+	'''
+	compute the modeled stress part coming from the derivation of the energy expression. We do not add here the hydrostatic pressure influence
+	(caution : the HP is mandatory to ensure a free stress state in a non deformed configuration)
+	needs revision ..... in order to include the HP. 
+	'''
+	strain=_toBase(strain, theta)
+	invstrain=_toBase(invstrain, theta)
+	sig=sum([p[0]*_approxLangevin(_computeDirectionStrain(strain,u[:,i])/np.sqrt(p[1]),'Boyce')*_computeDeriveDirectionStrain(strain, u[:,i]) for i in range(u.shape[1])])
+	
+	return invstrain, sig
+
 
 def model_MR2(element,C1,C2): 
 	''' 
@@ -87,57 +99,58 @@ def vi_struct(strain, theta, u1,u2,u3):
 	return vu
 
 
-
+strain_=np.arange(1.0,1.7,0.01)
+stress_=model_MR2(strain_,0.014,0.0007)
 ### material direction definition
 ''' should be submitted as a vector of the 3 coordinates of the direction in 3D and 2 in 2D case
 '''
-fichier = open('data.txt', 'a')
-a=np.loadtxt('directions.csv',dtype=str,delimiter=',',usecols=(0,1,2,3,4),skiprows=1) #les 110 directions qui mappent la sphère
-u1=np.transpose(a[0:,1].astype(np.float64).tolist())
-u2=np.transpose(a[0:,2].astype(np.float64).tolist())
-u3=np.transpose(a[0:,3].astype(np.float64).tolist())
-wi=np.transpose(a[0:,4].astype(np.float64).tolist())
+#fichier = open('data.txt', 'a')
+#a=np.loadtxt('directions.csv',dtype=str,delimiter=',',usecols=(0,1,2,3,4),skiprows=1) #les 110 directions qui mappent la sphère
+#u1=np.transpose(a[0:,1].astype(np.float64).tolist())
+#u2=np.transpose(a[0:,2].astype(np.float64).tolist())
+#u3=np.transpose(a[0:,3].astype(np.float64).tolist())
+#wi=np.transpose(a[0:,4].astype(np.float64).tolist())
 
-v=np.array([u1,u2,u3])
-#u_axe11=np.array([1,0])
-#u_axe12=np.array([-1,0])
-#u_axe21=np.array([0,1])
-#u_axe22=np.array([0,-1])
-#u_axe31=np.array([np.cos(np.pi/4),np.sin(np.pi/4)])
-#u_axe32=np.array([-np.cos(np.pi/4),-np.sin(np.pi/4)])
-#u_axe41=np.array([np.cos(np.pi/4),-np.sin(np.pi/4)])
-#u_axe42=np.array([-np.cos(np.pi/4),np.sin(np.pi/4)])
+#v=np.array([u1,u2,u3])
+u_axe11=np.array([1,0])
+u_axe12=np.array([-1,0])
+u_axe21=np.array([0,1])
+u_axe22=np.array([0,-1])
+u_axe31=np.array([np.cos(np.pi/4),np.sin(np.pi/4)])
+u_axe32=np.array([-np.cos(np.pi/4),-np.sin(np.pi/4)])
+u_axe41=np.array([np.cos(np.pi/4),-np.sin(np.pi/4)])
+u_axe42=np.array([-np.cos(np.pi/4),np.sin(np.pi/4)])
 
-#u=np.transpose(np.array([u_axe11,u_axe12,u_axe21,u_axe22,u_axe31,u_axe32,u_axe41,u_axe42]))
-		   
-strain_=np.arange(1.0,1.7,0.01)
-stress_=model_MR2(strain_,0.014,0.0007)
+u=np.transpose(np.array([u_axe11,u_axe12,u_axe21,u_axe22,u_axe31,u_axe32,u_axe41,u_axe42]))
 
+
+
+wi=1./u.shape[1]
 
 #stress_soll=_toTensor(stress,0,0)
 #strain_soll=_toTensor(strain,0,1/np.sqrt(strain))
 
-def err_matrice(p,stress=stress_, strain=strain_, u=v, wi=wi):
+def err_matrice(p,stress=stress_, strain=strain_, u=u):
 	res_sens1=np.zeros_like(stress_)
 	res_sens2=np.zeros_like(stress_)
 	for j in range(len(strain)):
-		strain=_toTensor(strain_[j], 0, 1/np.sqrt(strain_[j]), 1/np.sqrt(strain_[j]) ) ## def of strain tens at j
+		strain=_toTensor(strain_[j], 0, 1/np.sqrt(strain_[j])) ## def of strain tens at j
 		invstrain=np.linalg.inv(strain)
 		stress=_toTensor(stress_[j],0,0)  ### def of stress tens at instant j
 		
 		### estimation of the stress seen by the material when sollicitation in direction 0°
-		strain0=_toBase(strain, 0,True)
-		invstrain0=_toBase(invstrain, 0,True)
+		strain0=_toBase(strain, 0)
+		invstrain0=_toBase(invstrain, 0)
 		stress0=_toBase(stress, 0)
-		sig0_11=sum([(wi[i])*p[0]*_approxLangevin(_computeDirectionStrain(strain0,u[:,i])/np.sqrt(p[1]),'Boyce')*_computeDeriveDirectionStrain(strain0, u[:,i]) for i in range(u.shape[1])])
+		sig0_11=sum([p[0]*_approxLangevin(_computeDirectionStrain(strain0,u[:,i])/np.sqrt(p[1]),'Boyce')*_computeDeriveDirectionStrain(strain0, u[:,i]) for i in range(u.shape[1])])
 		press=sig0_11[1,1]/invstrain0[1,1] ## hydrostatic pression determination
 		sig0=(sig0_11-press*invstrain0)[0,0]
 
 		### estimation of the stress seen by the material when sollicitation in direction 90°
-		strain90=_toBase(strain, np.pi/2., True)
-		invstrain90=_toBase(invstrain, np.pi/2.,True)
+		strain90=_toBase(strain, np.pi/2.)
+		invstrain90=_toBase(invstrain, np.pi/2.)
 		stress90=_toBase(stress, np.pi/2.)
-		sig90_11=sum([(wi[i])*p[0]*_approxLangevin(_computeDirectionStrain(strain90,u[:,i])/np.sqrt(p[1]),'Boyce')*_computeDeriveDirectionStrain(strain90, u[:,i]) for i in range(u.shape[1])])
+		sig90_11=sum([p[0]*_approxLangevin(_computeDirectionStrain(strain90,u[:,i])/np.sqrt(p[1]),'Boyce')*_computeDeriveDirectionStrain(strain90, u[:,i]) for i in range(u.shape[1])])
 		press=sig90_11[0,0]/invstrain90[0,0] ## hydrostatic pression determination
 		sig90=(sig90_11-press*invstrain90)[1,1]
 		
@@ -146,77 +159,94 @@ def err_matrice(p,stress=stress_, strain=strain_, u=v, wi=wi):
 
 	return sum(res_sens1*res_sens1+res_sens2*res_sens2)
 
-u=v
+
+
 
 bounds_matrix=((0,None),(0,None))
-p0=[10,10]
+p0=[1,10]
 
-P_dragon=opt.minimize(err_matrice,p0,args=(stress_, strain_,u,wi),method='SLSQP')
+P_dragon=opt.minimize(err_matrice,p0,args=(stress_, strain_,u),method='SLSQP')
 coeff_dragon=P_dragon.x
-p=coeff_dragon
-
+pdrag=coeff_dragon
+print 'coeff dragon : ', pdrag
 contrainte_mod=np.zeros_like(strain_)
 contrainte_mod2=np.zeros_like(strain_)
 
 for j in range(len(strain_)):
-	strain=_toTensor(strain_[j], 0, 1/np.sqrt(strain_[j]),1/np.sqrt(strain_[j])) ## def of strain tens at j
+	strain=_toTensor(strain_[j], 0, 1/np.sqrt(strain_[j])) ## def of strain tens at j
 	invstrain=np.linalg.inv(strain)
 	stress=_toTensor(stress_[j],0,0)  ### def of stress tens at instant j
 	
 	### estimation of the stress seen by the material when sollicitation in direction 0°
-	strain0=_toBase(strain, 0, True)
-	invstrain0=_toBase(invstrain, 0,True)
+	strain0=_toBase(strain, 0)
+	invstrain0=_toBase(invstrain, 0)
 	stress0=_toBase(stress, 0)
-	sig0_11=sum([(wi[i])*p[0]*_approxLangevin(_computeDirectionStrain(strain0,u[:,i])/np.sqrt(p[1]),'Boyce')*_computeDeriveDirectionStrain(strain0, u[:,i]) for i in range(u.shape[1])])
+	sig0_11=sum([pdrag[0]*_approxLangevin(_computeDirectionStrain(strain0,u[:,i])/np.sqrt(pdrag[1]),'Boyce')*_computeDeriveDirectionStrain(strain0, u[:,i]) for i in range(u.shape[1])])
 	press=sig0_11[1,1]/invstrain0[1,1] ## hydrostatic pression determination
 	sig0=(sig0_11-press*invstrain0)[0,0]
 	contrainte_mod[j]=sig0
 	
-	strain90=_toBase(strain, np.pi/2, True)
-	invstrain90=_toBase(invstrain, np.pi/2., True)
+	strain90=_toBase(strain, np.pi/2)
+	invstrain90=_toBase(invstrain, np.pi/2.)
 	stress90=_toBase(stress, np.pi/2.)
-	sig90_11=sum([(wi[i])*p[0]*_approxLangevin(_computeDirectionStrain(strain90,u[:,i])/np.sqrt(p[1]),'Boyce')*_computeDeriveDirectionStrain(strain90, u[:,i]) for i in range(u.shape[1])])
+	sig90_11=sum([pdrag[0]*_approxLangevin(_computeDirectionStrain(strain90,u[:,i])/np.sqrt(pdrag[1]),'Boyce')*_computeDeriveDirectionStrain(strain90, u[:,i]) for i in range(u.shape[1])])
 	press=sig90_11[0,0]/invstrain90[0,0] ## hydrostatic pression determination
 	sig90=(sig90_11-press*invstrain90)[1,1]
 	contrainte_mod2[j]=sig90
 
-	
-plt.plot(strain_, stress_,'b', linewidth=5);plt.plot(strain_, contrainte_mod,'r--', linewidth=5);plt.plot(strain_, contrainte_mod2, 'g')
+plt.figure()
+plt.plot(strain_, stress_,'b', linewidth=2);plt.plot(strain_, contrainte_mod,'r--', linewidth=2);plt.plot(strain_, contrainte_mod2, 'g')
+plt.plot(strain_, (stress_-contrainte_mod)/stress_, 'g', linewidth=3)
+plt.plot(strain_, (stress_-contrainte_mod2)/stress_, 'g--', linewidth=3)
 
+plt.grid()
+plt.title('Dragon')
 
 stress_=model_MR2(strain_,0.086,0.003)
 bounds_matrix=((0,None),(0,None))
-p0=[10,10]
+cons = (
+	#{'type' : 'ineq','fun' : lambda x : np.array(x[1]-2*x[0]/3.)},
+	{'type' : 'eq','fun' : lambda x : np.array(x[0]-pdrag[0])}
+	#{'type' : 'ineq','fun' : lambda x : np.array(x[1]-0.05)}
+	)
 
-P_dragon=opt.minimize(err_matrice,p0,args=(stress_, strain_,u,wi),method='SLSQP')
-coeff_dragon=P_dragon.x
-p=coeff_dragon
+p0=[pdrag[0],10]
+
+P_sc37=opt.minimize(err_matrice,p0,args=(stress_, strain_,u),method='SLSQP',bounds=bounds_matrix)
+coeff_sc37=P_sc37.x
+p=coeff_sc37
+
+print 'coeff SC37 : ', p
 
 contrainte_mod=np.zeros_like(strain_)
 contrainte_mod2=np.zeros_like(strain_)
 
 for j in range(len(strain_)):
-	strain=_toTensor(strain_[j], 0, 1/np.sqrt(strain_[j]),1/np.sqrt(strain_[j])) ## def of strain tens at j
+	strain=_toTensor(strain_[j], 0, 1/np.sqrt(strain_[j])) ## def of strain tens at j
 	invstrain=np.linalg.inv(strain)
 	stress=_toTensor(stress_[j],0,0)  ### def of stress tens at instant j
 	
 	### estimation of the stress seen by the material when sollicitation in direction 0°
-	strain0=_toBase(strain, 0, True)
-	invstrain0=_toBase(invstrain, 0,True)
+	strain0=_toBase(strain, 0)
+	invstrain0=_toBase(invstrain, 0)
 	stress0=_toBase(stress, 0)
-	sig0_11=sum([(wi[i])*p[0]*_approxLangevin(_computeDirectionStrain(strain0,u[:,i])/np.sqrt(p[1]),'Boyce')*_computeDeriveDirectionStrain(strain0, u[:,i]) for i in range(u.shape[1])])
+	sig0_11=sum([p[0]*_approxLangevin(_computeDirectionStrain(strain0,u[:,i])/np.sqrt(p[1]),'Boyce')*_computeDeriveDirectionStrain(strain0, u[:,i]) for i in range(u.shape[1])])
 	press=sig0_11[1,1]/invstrain0[1,1] ## hydrostatic pression determination
 	sig0=(sig0_11-press*invstrain0)[0,0]
 	contrainte_mod[j]=sig0
 	
-	strain90=_toBase(strain, np.pi/2, True)
-	invstrain90=_toBase(invstrain, np.pi/2., True)
+	strain90=_toBase(strain, np.pi/2)
+	invstrain90=_toBase(invstrain, np.pi/2.)
 	stress90=_toBase(stress, np.pi/2.)
-	sig90_11=sum([(wi[i])*p[0]*_approxLangevin(_computeDirectionStrain(strain90,u[:,i])/np.sqrt(p[1]),'Boyce')*_computeDeriveDirectionStrain(strain90, u[:,i]) for i in range(u.shape[1])])
+	sig90_11=sum([p[0]*_approxLangevin(_computeDirectionStrain(strain90,u[:,i])/np.sqrt(p[1]),'Boyce')*_computeDeriveDirectionStrain(strain90, u[:,i]) for i in range(u.shape[1])])
 	press=sig90_11[0,0]/invstrain90[0,0] ## hydrostatic pression determination
 	sig90=(sig90_11-press*invstrain90)[1,1]
 	contrainte_mod2[j]=sig90
 	
-	
-plt.plot(strain_, stress_,'b', linewidth=5);plt.plot(strain_, contrainte_mod,'r--', linewidth=5);plt.plot(strain_, contrainte_mod2, 'g')
+plt.figure()
+plt.plot(strain_, stress_,'b', linewidth=3);plt.plot(strain_, contrainte_mod,'r--', linewidth=3);plt.plot(strain_, contrainte_mod2, 'g')
+plt.plot(strain_, (stress_-contrainte_mod)/stress_, 'g', linewidth=3)
+plt.plot(strain_, (stress_-contrainte_mod2)/stress_, 'k--', linewidth=3)
 
+plt.grid()
+plt.title('SC37')
